8721b2767a188e370a222dde49f6605d95fab049
Giovanni Palma <palma.giovanni@spes.uniud.it>
2023-05-10 23:49:10 +0200

    Interfaccia semplice per la creazione di blocchi custom

---
+++ bukkit/src/main/java/me/dragospiro98/castle/bukkit/data/blocks/CustomBlock.java
@@ -0,0 +1,8 @@
+package me.dragospiro98.castle.bukkit.data.blocks;
+
+import me.dragospiro98.castle.bukkit.CastlePlugin;
+import org.bukkit.inventory.ItemStack;
+
+public interface CustomBlock {
+    public ItemStack build(CastlePlugin plugin);
+}
