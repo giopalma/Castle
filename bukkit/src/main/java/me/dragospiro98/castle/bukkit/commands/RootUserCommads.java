e02017430efd2af4ec1aa47db7eaa17a45759700
Giovanni Palma <palma.giovanni@spes.uniud.it>
2023-05-10 23:50:53 +0200

    Utilizzo del BlockBuilder per dare al player il blocco custom GuardianSpawner

--- bukkit/src/main/java/me/dragospiro98/castle/bukkit/commands/RootUserCommads.java
+++ bukkit/src/main/java/me/dragospiro98/castle/bukkit/commands/RootUserCommads.java
@@ -3,6 +3,8 @@ import dev.jorel.commandapi.CommandAPICommand;
 import me.dragospiro98.castle.bukkit.CastlePlugin;
 import me.dragospiro98.castle.bukkit.data.User;
 import me.dragospiro98.castle.bukkit.data.blocks.BlockBuilder;
+import me.dragospiro98.castle.bukkit.data.blocks.CustomBlockTypes;
+import me.dragospiro98.castle.bukkit.data.blocks.GuardianSpawner;
 import me.dragospiro98.castle.bukkit.managers.PlayerManager;
 import me.dragospiro98.castle.bukkit.messages.Message;
 import org.bukkit.Material;
@@ -16,57 +18,48 @@ import java.util.List;
 public class RootUserCommads {
     private CastlePlugin plugin;
 
-    public RootUserCommads(CastlePlugin plugin){
-       this.plugin = plugin;
-   }
+    public RootUserCommads(CastlePlugin plugin) {
+        this.plugin = plugin;
+    }
 
-   public void runRootUserCommands(){
+    public void runRootUserCommands() {
         new CommandAPICommand("clan")
                 .withSubcommand(onCastleCommand())
                 .register();
-   }
+    }
 
-   private CommandAPICommand onCastleCommand(){
-       return new CommandAPICommand("castle")
-               .withPermission("parties.test.castle")
-               .withSubcommand(onCastleCreateCommand());
-   }
+    private CommandAPICommand onCastleCommand() {
+        return new CommandAPICommand("castle") // TODO: Cambiare comando base
+                .withPermission("parties.test.castle") // TODO: Cambiare permesso
+                .withSubcommand(onCastleCreateCommand());
+    }
 
-   private CommandAPICommand onCastleCreateCommand(){
+    private CommandAPICommand onCastleCreateCommand() {
         return new CommandAPICommand("create")
                 .executesPlayer(((player, args) -> {
                     User user = PlayerManager.getInstance().getPlayer(player.getUniqueId());
                     // Controllare se è in un clan
-                    if(!(user.isInClan())){
+                    if (!(user.isInClan())) {
                         user.sendMessage(Message.NOT_IN_CLAN);
                         return;
                     }
                     // Controllare se è leader del clan
-                    if(!(user.id.equals(user.getClan().getLeader()))){
+                    if (!(user.id.equals(user.getClan().getLeader()))) {
                         user.sendMessage(Message.NOT_LEADER);
                         return;
                     }
                     // Controllare se il giocatore ha l'inventario pieno
-                    if (player.getInventory().firstEmpty() == -1){
+                    if (player.getInventory().firstEmpty() == -1) {
                         user.sendMessage(Message.FULL_INVENTORY);
                         return;
                     }
                     // Dare il blocco dorato al giocatore, il blocco non si può buttare, bisogna controllare se ha spazio nell'inventario
-                    ItemStack guardian_spawner = getGuardianSpawner();
+                    ItemStack guardian_spawner = new BlockBuilder(CustomBlockTypes.GUARDIAN_SPAWNER,plugin).build();
                     player.getInventory().addItem(guardian_spawner);
                     // Creare un comando /clan castle cancel per annullare l'operazione, rimuovere il blocco dorato.
                     // Al posizionamento del blocco dorato si deve creare un claim
 
                     // si deve posizionare un guardiano che attacca tutti i nemici
                 }));
-   }
-
-   private ItemStack getGuardianSpawner(){
-        return new BlockBuilder(Material.GOLD_BLOCK)
-                .name(plugin.getConfig().getString("messages.guardian_spawner.block_name"))
-                .lore(plugin.getConfig().getStringList("messages.guardian_spawner.description"))
-                .enchant(Enchantment.WATER_WORKER,1,true)
-                .itemFlag(ItemFlag.HIDE_ENCHANTS)
-                .build();
-   }
+    }
 }
