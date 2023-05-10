c858f42fefea56c0792ac823319ad3f4f691a363
Giovanni Palma <palma.giovanni@spes.uniud.it>
2023-05-11 00:00:02 +0200

    Creazione del blocco

--- bukkit/src/main/java/me/dragospiro98/castle/bukkit/data/blocks/AdminConfigBlock.java
+++ bukkit/src/main/java/me/dragospiro98/castle/bukkit/data/blocks/AdminConfigBlock.java
@@ -2,13 +2,20 @@ package me.dragospiro98.castle.bukkit.data.blocks;
 
 import me.dragospiro98.castle.bukkit.CastlePlugin;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import java.util.Arrays;
 
 public class AdminConfigBlock implements CustomBlock{
 
     @Override
     public ItemStack build(CastlePlugin plugin) {
-        return new BlockBuilder(Material.GOLD_BLOCK)
+        return new BlockBuilder(Material.PRISMARINE_BRICKS)
+                .name("CASTLE CONFIGURE")
+                .lore(Arrays.asList("Posiziona questo blocco", "per creare la region della fortezza")) //TODO: Leggerli dal file config
+                .enchant(Enchantment.OXYGEN,1,true)
+                .itemFlag(ItemFlag.HIDE_ENCHANTS)
                 .build();
     }
 }
